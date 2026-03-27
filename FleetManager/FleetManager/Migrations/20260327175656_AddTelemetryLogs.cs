e79cd86a08b367aebf5874b482e24d56adfb6c94
Lukasz <s096087@student.tu.kielce.pl>
2026-03-27 19:09:21 +0100

    migracja: utworzenie nowej tabeli TelemetryLogs - przygotowanie struktury bazy danych do ewentualnej integracji z czujnikami telemetrycznymi

---
+++ FleetManager/FleetManager/Migrations/20260327175656_AddTelemetryLogs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTelemetryLogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TelemetryLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    VehicleId = table.Column<int>(type: "int", nullable: false),
+                    Latitude = table.Column<double>(type: "double", nullable: false),
+                    Longitude = table.Column<double>(type: "double", nullable: false),
+                    SpeedKph = table.Column<double>(type: "double", nullable: true),
+                    FuelLevel = table.Column<double>(type: "double", nullable: true),
+                    Timestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    RowVersion = table.Column<DateTime>(type: "timestamp(6)", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TelemetryLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TelemetryLogs_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TelemetryLogs_VehicleId",
+                table: "TelemetryLogs",
+                column: "VehicleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TelemetryLogs");
+        }
+    }
+}
