8e2ec47708b10bfcb2f3a2f45494730a4a0d1247
Lukasz <s096087@student.tu.kielce.pl>
2026-06-05 12:45:18 +0200

    fix: exclude ad-hoc events from prediction logic

---
+++ FleetManager/FleetManager/Migrations/20260605090825_UpdateOtherMaintenanceTypeSeedData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOtherMaintenanceTypeSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MaintenanceTypes",
+                keyColumn: "Id",
+                keyValue: 99,
+                columns: new[] { "DefaultIntervalDays", "DefaultIntervalOdometer" },
+                values: new object[] { null, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MaintenanceTypes",
+                keyColumn: "Id",
+                keyValue: 99,
+                columns: new[] { "DefaultIntervalDays", "DefaultIntervalOdometer" },
+                values: new object[] { 0, 0 });
+        }
+    }
+}
